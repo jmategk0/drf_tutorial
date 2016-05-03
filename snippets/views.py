eaf1f3faceaec2a74047a157c44537cabebb7ff4
jmategk0 <jmategkox2@gmail.com>
2016-05-03 15:56:36 -0700

    Update ViewSets to use routers.

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,6 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
-from rest_framework import generics
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
