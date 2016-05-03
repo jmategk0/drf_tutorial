528e55823627398fdb5bac12865b71d110e64231
jmategk0 <jmategkox2@gmail.com>
2016-05-03 14:29:43 -0700

    Update to add relationship hyperlinks and browsable API

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -18,8 +18,3 @@ from django.conf.urls import url, include
 urlpatterns = [
     url(r'^', include('snippets.urls')),
 ]
-
-urlpatterns += [
-    url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='rest_framework')),
-]
