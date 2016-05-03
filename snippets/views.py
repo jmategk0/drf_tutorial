92b0da9a18b052e6ca3d168542bee81f9ffa2ca7
jmategk0 <jmategkox2@gmail.com>
2016-05-03 16:07:18 -0700

    Cleanup code.

--- snippets/views.py
+++ snippets/views.py
@@ -4,22 +4,12 @@ from snippets.serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 
 # Create your views here.
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
